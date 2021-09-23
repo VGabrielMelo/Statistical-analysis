2a3ab1fdac23743b6a5982f901684a1a00d774d8
NicholasRoque <nicholas.sroque@gmail.com>
2021-09-23 20:08:34 -0300

    teste

--- frontend/src/js/Upload/index.js
+++ frontend/src/js/Upload/index.js
@@ -1,14 +1,37 @@
 import api from './../../services/api'
+import getFileExtension from './../../utils/getFileExtension'
+
 
 async function uploadArquivo(arquivo){
-    let data = {
-        "dataset":arquivo
+    let extensao = getFileExtension(arquivo)
+    let rota
+    if(extensao==="csv"){
+        rota="/up-csv"
+    }else{
+        rota="/up-exel"
     }
-    await api.post("/up-csv",data).then((res)=>{
+    let caminho = arquivo.split(".")
+    caminho.pop()
+    caminho = caminho.toString()
+    let data = {
+        "dataset":caminho
+    }
+    console.log(data)
+    await api.post(rota,data).then((res)=>{
         console.log(res)
     }).catch((err)=>{
         console.log(err)
     })
+
+    /* async function uploadArquivo(arquivo){
+        console.log(arquivo instanceof File)
+        const formData = new FormData()
+        formData.append('arquivo', arquivo)
+        await api.post("/upload",formData).then((res)=>{
+            console.log(res)
+        }).catch((err)=>{
+            console.log(err)
+        }) */
 }
 
 export default uploadArquivo;
--- frontend/src/pages/Upload/index.js
+++ frontend/src/pages/Upload/index.js
@@ -24,12 +24,9 @@ function Upload(){
                     <Form.Label>Faça o upload do arquivo.</Form.Label>
                     <Form.Control id="arquivo" name="arquivo" type="file" />
                 </Form.Group> */}
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="formGroupArquivo">
                     <Form.Label>Entre com o endereço do arquivo</Form.Label>
-                    <Form.Control type="text" placeholder="C://arquivo/exemplo" />
-                    <Form.Text className="text-muted">
-                        Colocar arquivo sem a extensão
-                    </Form.Text>
+                    <Form.Control type="text" id="arquivo" placeholder="C://arquivo/exemplo" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Enviar
@@ -40,3 +37,40 @@ function Upload(){
 }
 
 export default Upload;
+
+/* 
+import { React, useState } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+import uploadArquivo from './../../js/Upload'
+import $ from 'jquery'
+import "./index.css"
+
+function Upload(){
+    // ./../../../../assets/files
+    const [arquivo,setArquivo] = useState()
+    const uploadFile = (e) =>{
+        e.preventDefault()
+        uploadArquivo(arquivo).then(()=>{
+        })
+        .catch(()=>{
+
+        })
+    }
+    return (
+        <Container id="upload">
+            <Form encType="multipart/form-data" onSubmit={uploadFile}>
+                <Form.Group controlId="formFile" className="mb-3">
+                    <Form.Label>Faça o upload do arquivo.</Form.Label>
+                    <Form.Control onChange={(e)=>setArquivo(e.target.files[0])} id="arquivo" name="arquivo" type="file" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Enviar
+                </Button>
+            </Form>
+        </Container>
+    )
+}
+
+export default Upload;
+ */
